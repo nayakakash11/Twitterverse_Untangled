baf7852e7772b460dd17da08a5588ebf0b009864
nayakakash11 <akashsnayak2003@gmail.com>
2024-04-24 12:57:44 +0530

    changes

---
+++ Connected_users.scala
@@ -0,0 +1,17 @@
+import org.apache.spark.graphx.GraphLoader
+import org.apache.spark.graphx._
+import org.apache.spark.sql.functions._
+
+val graph = GraphLoader.edgeListFile(sc, "/Users/akashnayak/Documents/higgs-social_network.edgelist")
+
+val cc = graph.connectedComponents().vertices
+
+val users = sc.textFile("/Users/akashnayak/Documents/Foll_ID.txt").map {line => val fields = line.split(" ")(fields(0).toLong, fields(1))}
+
+val join = users.join(cc).map {case (id, (username, cc)) => (username, cc)}
+
+val grp = join.groupBy{ case(_, (cc)) => cc}.mapValues(iter => iter.size)
+
+val top10 = grp.top(10)(Ordering.by(_._2))
+
+top10 foreach {case (cc, size) => println(s"Component ID: $cc, Size: $size")}
---
+++ Most_influential_users.scala
@@ -0,0 +1,12 @@
+import org.apache.spark.graphx.GraphLoader
+import org.apache.spark.graphx._
+import org.apache.spark.sql.functions._
+import org.apache.spark.graphx.lib._
+
+val graph = GraphLoader.edgeListFile(sc, "/Users/akashnayak/Documents/higgs-social_network.edgelist")
+
+val degreeCentrality = graph.degrees
+
+val top10 = degreeCentrality.top(10)(Ordering.by(_._2))
+
+top10.foreach {case (cc, dc) => println(s"User Id: $cc, Degree Centrality: $dc")}
