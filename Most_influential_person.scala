bd363691219e0f9390658e9887a8624b5558f28f
AKASH KUMAR NAYAK <akashsnayak2003@gmail.com>
2024-04-30 11:12:59 +0530

    correction

--- Most_influential_person.scala
+++ Most_influential_person.scala
@@ -6,6 +6,6 @@ val graph = GraphLoader.edgeListFile(sc, "social_network.edgelist")
 
 val pageRank = graph.pageRank(0.85).vertices
 
-val mostPopularPerson = pageRank.max()(Ordering.by(_._2))
+val mostInfluentialPerson = pageRank.max()(Ordering.by(_._2))
 
-println(s"The most popular person is User ${mostPopularPerson._1} with PageRank ${mostPopularPerson._2}")
+println(s"The most influential person is User ${mostInfluentialPerson._1} with PageRank ${mostInfluentialPerson._2}")
