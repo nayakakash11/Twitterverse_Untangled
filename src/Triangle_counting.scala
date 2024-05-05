94b9e0056e4c5ca85a17e4dac0638bcd6880cd54
Aditya Raj <71901647+adityaraj30@users.noreply.github.com>
2024-05-05 17:18:46 +0530

    Update Triangle_counting.scala

--- src/Triangle_counting.scala
+++ src/Triangle_counting.scala
@@ -4,11 +4,11 @@ import org.apache.spark.sql.functions._
 import org.apache.spark.graphx.lib._
 
 val time0 = System.currentTimeMillis()
-val graph = GraphLoader.edgeListFile(sc, "social_network.edgelist", true).partitionBy(PartitionStrategy.RandomVertexCut)
+val graph = GraphLoader.edgeListFile(sc, "hdfs://nn.xxxxxxxxx:port_no/social_network.txt", true).partitionBy(PartitionStrategy.RandomVertexCut)
 
 val triCounts = graph.triangleCount().vertices
 
-val users = sc.textFile("Twitter_Id.txt").map { line =>
+val users = sc.textFile("hdfs://nn.xxxxxxxxx:port_no/Twitter_Id.txt").map { line =>
   line.split(" ") match { case Array(id, name) => (id.toLong, name.toInt) } }
 
 val triCountByUsername = users.join(triCounts).map { case (id, (username, tc)) => (username, tc) }
