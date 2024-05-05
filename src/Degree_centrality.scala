2d1e6285646d040824bb18e2cc29f83842615a96
Aditya Raj <71901647+adityaraj30@users.noreply.github.com>
2024-05-05 17:17:42 +0530

    Update Degree_centrality.scala

--- src/Degree_centrality.scala
+++ src/Degree_centrality.scala
@@ -3,7 +3,7 @@ import scala.collection.mutable.HashMap
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.graphx._
 
-val graph = GraphLoader.edgeListFile(sc, "social_network.edgelist")
+val graph = GraphLoader.edgeListFile(sc, "hdfs://nn.xxxxxxxxx:port_no/social_network.txt")
 
 val normalizationFactor:Float = 1f/(graph.vertices.count()-1)
 
@@ -11,7 +11,7 @@ val degrees: VertexRDD[Int] = graph.degrees.persist()
 
 val normalized = degrees.map((s => (s._1, s._2*normalizationFactor)))
 
-val users = sc.textFile("Twitter_Id.txt").map { line => line.split(" ") match {
+val users = sc.textFile("hdfs://nn.xxxxxxxxx:port_no/Twitter_Id.txt").map { line => line.split(" ") match {
   case Array(id, name) => (id.toLong, name.toInt) } }
 
 val ranksByUsername = users.join(normalized).map { case (id, (username, score)) => (username, score) }
